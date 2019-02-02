6a7febe162f8bc6efd47e82d6373b8f158e2fdcf
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-02-02 20:32:29 +0200

    Support for new file types
    Added support for all file types on all websites, added option to choose file name and file type (Example: zip, txt).
    Added support for using tool from command line

--- MinGitLoad.cmd
+++ MinGitLoad.cmd
@@ -8,11 +8,33 @@ cd %~dp0
 :: Turns on powershell scripts for current user ::
 powershell.exe Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force;
 
+IF [%1] == [] goto :ask
+IF [%2] == [] goto :ask
+IF [%3] == [] goto :ask
+IF [%4] == [] goto :ask
+
+:: IF values are given already, skips asking for values ::
+set repo=%1
+set dir=%2
+set file_name=%3
+set file_type=%4
+
+goto git
+:ask
+
 :: Asks for Link to repo ::
 set /p repo=Clone: 
 
 :: Asks for destination ::
-set /p dir=Download Path: 
+set /p dir=Download Path: ´
+
+:: Asks for file name ::
+set /p file_name=File Name: 
+
+:: Asks for file type (example: txt, zip) ::
+set /p file_type=File Type (Example: txt, zip): 
+
+:git
 
 :: Creates Temporary Directory ::
 md tmp >nul
@@ -21,16 +43,16 @@ md tmp >nul
 copy download.ps1 tmp\download.ps1 >nul
 
 :: Adds Repo Link and Destination Path to tmp\Download.ps1 ::
-echo download %cd%\tmp\master.zip %repo% >> tmp\download.ps1
+echo download %cd%\tmp\%file_name%.%file_type% %repo% >> tmp\download.ps1
 
 :: Runs Downloader ::
 powershell.exe -File tmp\download.ps1
 
 :: Copies Downloaded file to Destination Path
-copy %cd%\tmp\master.zip %dir% /Y >nul
+copy %cd%\tmp\%file_name%.%file_type% %dir% /Y >nul
 
 :: Deleted Temporary Files
-del /F /Q tmp\master.zip
+del /F /Q tmp\%file_name%.%file_type%
 del /F /Q tmp\download.ps1
 
 :: Deletes tmp Folder ::
--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Minimalistic version of https://www.github.com/Y4hL/GitLoad
 Basic ``git clone`` for Windows  
   
 Only uses built in Windows file types  
+Works on any type of file on any website
+
+Format:
+
+``MinGitLoad.cmd website save_directory file_name file_type``
+
+Example:
+
+``MinGitLoad.cmd github.com/y4hl/mingitload %userprofile%\desktop Package zip``
+
+Can be entered manually by just opening MinGitLoad.cmd
+
+For downloading github repos, file_type should always be ``zip``
