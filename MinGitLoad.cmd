9c64a45816a30a635eb2350d0db5592ffb4575ca
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-02-15 20:10:28 +0200

    Variables rename
    Trying to switch variables to less used names, to reduce overwriting variables when using the code with the "call" command

--- MinGitLoad.cmd
+++ MinGitLoad.cmd
@@ -1,9 +1,6 @@
 @echo off
 title Y4hL/MinGitLoad
 
-:: Assigns Current Files Directory to a variable ::
-set file_dir=%~dp0
-
 :: Assigns random number to a variable ::
 set tmprandom=%random%
 
@@ -15,88 +12,88 @@ IF [%2] == [] goto :ask
 IF [%3] == [] (
 
     :: Sets file name and type automatically, if no name or type is given ::
-    set file_type=%~x1
-    set file_name=%~n1
+    set mingitload_file_type=%~x1
+    set mingitload_file_name=%~n1
 
     goto :auto
 )
 IF [%4] == [] (
 
     :: Sets file type automatically, if no type is given ::
-    set file_type=%~x1
+    set mingitload_file_type=%~x1
 
     goto :name
 )
 
 :: IF values are given already, skips asking for values ::
-set file_type=.%4
+set mingitload_file_type=.%4
 :name
-set file_name=%3
+set mingitload_file_name=%3
 :auto
-set dir=%2
-set repo=%1
+set mingitload_dir=%2
+set mingitload_repo=%1
 
 
 goto git
 :ask
 
-:: Asks for Link to repo ::
-set /p repo=Clone: 
+:: Asks for Link to Repo ::
+set /p mingitload_repo=Clone: 
 
 :: Asks for destination ::
-set /p dir=Download Path: 
+set /p mingitload_dir=Download Path: 
 
 :: Asks for file name ::
-set /p file_name=File Name: 
+set /p mingitload_file_name=File Name: 
 
 :: Asks for file type (example: txt, zip) ::
-set /p file_type=File Type (Example: txt, zip): 
+set /p mingitload_file_type=File Type (Example: txt, zip): 
 
 
 :git
 
 
-:: Removed Double Quotes from repo parameter ::
-set repo=%repo:"=%
-:: Removed Single Quotes from repo parameter ::
-set repo=%repo:'=%
+:: Removed Double Quotes from Repo parameter ::
+set mingitload_repo=%mingitload_repo:"=%
+:: Removed Single Quotes from Repo parameter ::
+set mingitload_repo=%mingitload_repo:'=%
 
-:: Removed Double Quotes from dir parameter ::
-set dir=%dir:"=%
-:: Removed Single Quotes from dir parameter ::
-set dir=%dir:'=%
+:: Removed Double Quotes from mingitload_dir parameter ::
+set mingitload_dir=%mingitload_dir:"=%
+:: Removed Single Quotes from mingitload_dir parameter ::
+set mingitload_dir=%mingitload_dir:'=%
 
-:: Removes Double Quotes from file_name parameter ::
-set file_name=%file_name:"=%
-:: Removes Single Quotes from file_name paramter
-set file_name=%file_name:'=%
+:: Removes Double Quotes from mingitload_file_name parameter ::
+set mingitload_file_name=%mingitload_file_name:"=%
+:: Removes Single Quotes from mingitload_file_name paramter
+set mingitload_file_name=%mingitload_file_name:'=%
 
-:: Adds dot and removes Double Quotes from file_type paramter ::
-set file_type=.%file_type:"=%
-:: Removes Single Quotes from file_type parameter ::
-set file_type=%file_type:'=%
+:: Adds dot and removes Double Quotes from mingitload_file_type paramter ::
+set mingitload_file_type=.%mingitload_file_type:"=%
+:: Removes Single Quotes from mingitload_file_type parameter ::
+set mingitload_file_type=%mingitload_file_type:'=%
 
 
-If NOT exist "%dir%" md "%dir%"
+If NOT exist "%mingitload_dir%" md "%mingitload_dir%"
 
 :: Creates Temporary Directory ::
-md "%file_dir%\%tmprandom%" >nul
+md "%~dp0\%tmprandom%" >nul
 
 :: Copies Download file to %tmprandom% Folder ::
-copy "%file_dir%\download.ps1" "%file_dir%\%tmprandom%\download.ps1" >nul
+copy "%~dp0\download.ps1" "%~dp0\%tmprandom%\download.ps1" >nul
 
 :: Adds Repo Link and Destination Path to %tmprandom%\Download.ps1 ::
-echo download "%file_dir%\%tmprandom%\%file_name%%file_type%" "%repo%" >> "%file_dir%\%tmprandom%\download.ps1"
+echo download "%~dp0\%tmprandom%\%mingitload_file_name%%mingitload_file_type%" "%mingitload_repo%" >> "%~dp0\%tmprandom%\download.ps1"
 
 :: Runs Downloader ::
-powershell.exe -File "%file_dir%\%tmprandom%\download.ps1"
+powershell.exe -File "%~dp0\%tmprandom%\download.ps1"
 
 :: Copies Downloaded file to Destination Path ::
-copy "%file_dir%\%tmprandom%\%file_name%%file_type%" "%dir%" /Y >nul
+copy "%~dp0\%tmprandom%\%mingitload_file_name%%mingitload_file_type%" "%mingitload_dir%" /Y >nul
 
 :: Deleted Temporary Files ::
-del /F /Q "%file_dir%\%tmprandom%\%file_name%%file_type%"
-del /F /Q "%file_dir%\%tmprandom%\download.ps1"
+del /F /Q "%~dp0\%tmprandom%\%mingitload_file_name%%mingitload_file_type%"
+del /F /Q "%~dp0\%tmprandom%\download.ps1"
 
 :: Deletes %tmprandom% Folder ::
-rd /s /q "%file_dir%\%tmprandom%"
+rd /s /q "%~dp0\%tmprandom%"
